3276d28a5706aff5a075a5108b7439b0af642106
Adam Hrastnik <adam.hrastnik@gmail.com>
2022-11-01 19:21:29 +0100

    Simplified server example.

--- example_server.py
+++ example_server.py
@@ -8,13 +8,8 @@ SERVER_PORT: int = 6969
 
 
 class ExampleServer(DatagramProtocol):
-    def __init__(self, turn_ip: str, turn_port: int) -> None:
-        super().__init__()
-        ExampleServer.TURN_IP: str = turn_ip
-        ExampleServer.TURN_PORT: int = turn_port
-    
     def startProtocol(self):
-        self.transport.connect(ExampleServer.TURN_IP, ExampleServer.TURN_PORT)
+        self.transport.connect(YOUR_TURN_IP, YOUR_TURN_PORT)
         self.transport.write(b"reg")
 
     def datagramReceived(self, data, addr):
@@ -23,9 +18,10 @@ class ExampleServer(DatagramProtocol):
         self.transport.write(data)
 
     def connectionRefused(self):
-        print("Failed to reach your TURN relay!")
+        print("Failed to reach Your TURN relay!")
+        reactor.stop()
 
 
 if __name__ == "__main__":
-    reactor.listenUDP(SERVER_PORT, ExampleServer(YOUR_TURN_IP, YOUR_TURN_PORT))
+    reactor.listenUDP(SERVER_PORT, ExampleServer())
     reactor.run()
