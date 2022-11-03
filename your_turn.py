abe616fea03ced606eee0a7b6f282ff9a119b66e
Adam Hrastnik <adam.hrastnik@gmail.com>
2022-11-04 00:34:54 +0100

    Implemented message protocol & registration on server.

--- your_turn.py
+++ your_turn.py
@@ -1,30 +1,45 @@
 import struct
+from dataclasses import dataclass
 
 from twisted.internet import reactor
 from twisted.internet.protocol import DatagramProtocol
 
 YOUR_TURN_PORT: int = 6942
 
-# Packet structure: UDP<TURN<prefix: uint16, sender id: uint32, ENet packet>>
+# Packet structure: UDP<TURN<prefix: uint16, sender/receiver id: uint32, ENet packet>>
+# When registering by having no data inside ENet packet field
+# the ID field represents the sender, if there is data in the ENet packet, then the ID represents the receiver
 TURN_MSG_PREFIX: int = 0xAA
+TURN_MSG_PREAMBLE_LEN: int = 6
 
 
-def parse_turn_packet(self, turn_packet: bytes) -> bytes:
+def parse_turn_packet(turn_packet: bytes) -> bytes:
+    # Packet structure: UDP<TURN<prefix: uint16, sender id: uint32, ENet packet>>
+    # TODO: Strip address from received data
     # TODO: Verify sender id is valid
-    if len(turn_packet) <= 6:
+    if len(turn_packet) < TURN_MSG_PREAMBLE_LEN:
         return ()
     
-    preamble = struct.unpack(">HL", turn_packet[:6])
-    prefix, sender_id = preamble
+    preamble = struct.unpack(">HL", turn_packet[:TURN_MSG_PREAMBLE_LEN])
+    prefix, peer_id = preamble
     if prefix != TURN_MSG_PREFIX:
         return ()
     
-    return (sender_id, turn_packet[6:])
+    return (peer_id, turn_packet[TURN_MSG_PREAMBLE_LEN:])
 
 
-def make_turn_packet(self, id: int, enet_packet: bytes) -> bytes:
+def make_turn_packet(id: int, enet_packet: bytes = b"") -> bytes:
     preamble: bytes = struct.pack(">HL", TURN_MSG_PREFIX, id)
     return preamble + enet_packet
+
+
+@dataclass
+class YourTurnPeer:
+    ip: str
+    port: int
+
+    def get_addr(self) -> tuple:
+        return (self.ip, self.port)
 
 
 class YourTurnRelay(DatagramProtocol):
@@ -37,6 +52,8 @@ class YourTurnRelay(DatagramProtocol):
         # TODO: Add support for multiple clients
         self._client_ip: str = ""
         self._client_port: str = 0
+
+        self._peer_map: dict = {}
 
     def get_server_address(self) -> tuple:
         return (self._server_ip, self._server_port)
@@ -62,33 +79,45 @@ class YourTurnRelay(DatagramProtocol):
 
     def datagramReceived(self, data, addr) -> None:
         print(f"received {data.hex()} from {addr}")
-        self.transport.write(data, addr)
-        if not self.is_relay_linked():
-            # Attempt to register server
-            if data != b"reg":
-                return
-            
-            self._server_ip, self._server_port = addr
-            print(f"Server {self._server_ip}:{self._server_port} registered!")
+        sender_ip, sender_port = addr
+        peer_id, enet_packet = parse_turn_packet(data)
+        if len(enet_packet) == 0:
+            self.register_peer(peer_id, sender_ip, sender_port)
         else:
-            if self.is_server(addr):
-                # Data received from server - relay to client
-                client_addr: tuple = self.get_client_address()
-                if self.is_address_valid(client_addr):
-                    self.transport.write(data, client_addr)
-            else:
-                if not self.is_address_valid(self.get_client_address()):
-                    # Register client
-                    self._client_ip, self._client_port = addr
-                    print(f"Client {self._client_ip}:{self._client_port} registered!")
-                if self.is_client(addr):
-                    # Data received from client - relay to server
-                    self.transport.write(data, self.get_server_address())
+            peer: YourTurnPeer = self._peer_map.get(peer_id, None)
+            if peer is None:
+                return
+            self.transport.write(data, peer.get_addr())
+
+        # self.transport.write(data, addr)
+        # if not self.is_relay_linked():
+        #     # Attempt to register server
+        #     if data != b"reg":
+        #         return
+        #     # TODO: Register received peer port
+        #     self._server_ip, self._server_port = addr
+        #     print(f"Server {self._server_ip}:{self._server_port} registered!")
+        # else:
+        #     if self.is_server(addr):
+        #         # Data received from server - relay to client
+        #         client_addr: tuple = self.get_client_address()
+        #         if self.is_address_valid(client_addr):
+        #             self.transport.write(data, client_addr)
+        #     else:
+        #         if not self.is_address_valid(self.get_client_address()):
+        #             # Register client
+        #             self._client_ip, self._client_port = addr
+        #             print(f"Client {self._client_ip}:{self._client_port} registered!")
+        #         if self.is_client(addr):
+        #             # Data received from client - relay to server
+        #             self.transport.write(data, self.get_server_address())
     
-    # def register_server() -> None:
-    #     pass
+    def register_peer(self, id: int, ip: str, port: int) -> None:
+        # TODO: Handle cases where the peer already exists
+        self._peer_map[id] = YourTurnPeer(ip, port)
+        print(f"Peer {id}[{ip}:{port}] registered")
     
-    # def unregister_server() -> None:
+    # def unregister_peer(peer_id: int) -> None:
     #     pass
 
 
--- your_turn_middleman.py
+++ your_turn_middleman.py
@@ -4,39 +4,57 @@ from typing import Callable
 from twisted.internet import reactor
 from twisted.internet.protocol import DatagramProtocol
 
-from your_turn import YOUR_TURN_PORT
+from your_turn import (
+    YOUR_TURN_PORT,
+    parse_turn_packet,
+    make_turn_packet,
+)
 
 YOUR_TURN_IP: str = "127.0.0.1"
 
 
 class YourTurnMiddlemanInterface(DatagramProtocol):
-    def __init__(self, recv_callback: Callable, id: int = -1, port: int = 0) -> None:
+    def __init__(self, id: int, recv_callback: Callable, port: int = 0) -> None:
         super().__init__()
-        self._id = id
+
+        self._id: int = id
         self._port: int = port
         self._recv_callback: Callable = recv_callback
     
     def get_port(self) -> int:
         return self._port
-    
+
     def datagramReceived(self, data: bytes, addr: tuple) -> None:
         self._recv_callback(self._id, data, addr)
 
 
-class YourTurnMiddleman:
-    PACKET_PREFIX = 0xAA
-    PORT_RANGE_START: int = 6970
-    PEERS_MAX: int = 12  # TODO: Implement peer limit
+class YourTurnMiddlemanRelay(YourTurnMiddlemanInterface):
+    def startProtocol(self):
+        self.transport.connect(YOUR_TURN_IP, YOUR_TURN_PORT)
+        # Register interface on TURN server
+        self.transport.write(make_turn_packet(self._id))
 
-    def __init__(self, peer_id: int = -1) -> None:
-        self.relay = YourTurnMiddlemanInterface(self._received_from_relay)
+
+class YourTurnMiddlemanPeer(YourTurnMiddlemanInterface):
+    def startProtocol(self):
+        self.transport.connect("127.0.0.1", self._port)
+
+
+class YourTurnMiddleman:
+    PORT_RANGE_START: int = 6970
+    # TODO: Implement peer limit
+    # PEERS_MAX: int = 12
+
+    def __init__(self, id: int = 1) -> None:
+        self._id = id
+        self.relay = YourTurnMiddlemanRelay(id, self._received_from_relay)
         self._peers: dict = {}
         self._next_peer_port: int = YourTurnMiddleman.PORT_RANGE_START
         self.register_peer(1)
 
     def _received_from_relay(self, peer_id: int, turn_packet: bytes, addr: tuple) -> None:
         print(f"received {turn_packet.hex()} from {addr}")
-        parsed_turn_packet = self._parse_turn_packet(turn_packet)
+        parsed_turn_packet = parse_turn_packet(turn_packet)
         if parsed_turn_packet == ():
             print("Failed to parse TURN packet")
             return
@@ -47,20 +65,20 @@ class YourTurnMiddleman:
             peer = self.register_peer(peer_id)
         
         # Forward received data to peer
-        peer.transport.write(enet_packet, ("127.0.0.1", peer.get_port()))
+        peer.transport.write(enet_packet)
     
     def _received_from_peer(self, peer_id: int, enet_packet: bytes, addr: tuple) -> None:
         print(f"received {enet_packet.hex()} from {addr}")
-        turn_packet: bytes = self._make_turn_packet(peer_id, enet_packet)
+        turn_packet: bytes = make_turn_packet(peer_id, enet_packet)
         # Forward received data to relay server
-        self.relay.transport.write(turn_packet, (YOUR_TURN_IP, YOUR_TURN_PORT))
+        self.relay.transport.write(turn_packet)
     
-    def register_peer(self, peer_id: int) -> YourTurnMiddlemanInterface:
+    def register_peer(self, peer_id: int) -> YourTurnMiddlemanPeer:
         if peer_id <= 0 or peer_id in self._peers:
             return
         
         peer_port = self._next_peer_port
-        peer = YourTurnMiddlemanInterface(self._received_from_peer, id=peer_id, port=peer_port)
+        peer = YourTurnMiddlemanPeer(peer_id, self._received_from_peer, port=peer_port)
         self._peers[peer_id] = peer
         reactor.listenUDP(peer_port, peer)
         self._next_peer_port += 1
@@ -71,24 +89,6 @@ class YourTurnMiddleman:
     #     # TODO: Implement
     #     # TODO: Handle starting/stopping listeners
     #     pass
-
-    def _parse_turn_packet(self, turn_packet: bytes) -> bytes:
-        # Packet structure: UDP<TURN<prefix: uint16, sender id: uint32, ENet packet>>
-        # TODO: Strip address from received data
-        # TODO: Compare received id with assigned one
-        if len(turn_packet) <= 6:
-            return ()
-        
-        preamble = struct.unpack(">HL", turn_packet[:6])
-        prefix, sender_id = preamble
-        if prefix != YourTurnMiddleman.PACKET_PREFIX:
-            return ()
-        
-        return (sender_id, turn_packet[6:])
-
-    def _make_turn_packet(self, id: int, enet_packet: bytes) -> bytes:
-        preamble: bytes = struct.pack(">HL", YourTurnMiddleman.PACKET_PREFIX, id)
-        return preamble + enet_packet
     
     def run(self):
         reactor.listenUDP(0, self.relay)
