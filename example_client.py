c9d2f21f7d784d03819c569ba129f7f502aa0452
Adam Hrastnik <adam.hrastnik@gmail.com>
2022-11-06 00:10:06 +0100

    Added ping statistics display to the example Client.

--- example_client.py
+++ example_client.py
@@ -1,6 +1,8 @@
 import argparse
 import struct
 from time import perf_counter
+from collections import deque
+import statistics
 
 from twisted.internet import reactor, task
 from twisted.internet.protocol import DatagramProtocol
@@ -10,7 +12,8 @@ from your_turn_middleman import YourTurnMiddleman
 
 YOUR_TURN_IP: str = "127.0.0.1"
 MIDDLEMAN_IP: str = "127.0.0.1"
-PING_FREQUENCY: float = 1.0
+PING_DEFAULT_FREQUENCY: float = 100.0
+PING_STAT_PUBLISH: float = 1.0
 
 
 class ExampleClient(DatagramProtocol):
@@ -28,18 +31,23 @@ class ExampleClient(DatagramProtocol):
         self._bypass = bypass
         self._verbose = verbose
 
-        self._counter = 0
+        self._counter: int = 0
+        self._ping_buffer = deque(maxlen=2048)
 
     def startProtocol(self):
         self.transport.connect(*self._send_addr)
+
         if self._bypass:
             # Register on Relay
             self.transport.write(make_turn_packet(self._id))
-        # Send some data every second
+
         if self._ping_frequency > 0:
-            loop = task.LoopingCall(self.ping_server)
+            pinger = task.LoopingCall(self.ping_server)
             ping_delay = 1 / self._ping_frequency
-            loop.start(ping_delay, now=True)
+            pinger.start(ping_delay, now=True)
+
+            ping_statistics_publisher = task.LoopingCall(self.ping_publish_statistics)
+            ping_statistics_publisher.start(PING_STAT_PUBLISH, now=False)
 
     def datagramReceived(self, data, addr):
         if self._verbose:
@@ -65,7 +73,7 @@ class ExampleClient(DatagramProtocol):
         departure_time: float
         counter, departure_time = struct.unpack(">Lf100x", data)
         ping_ms: float = (perf_counter() - departure_time) * 1000
-        print(f"RTT [{counter}]:{ping_ms}\tms")
+        self._ping_buffer.append(ping_ms)
 
     def connectionRefused(self):
         print("Failed to reach Your TURN relay!")
@@ -80,6 +88,15 @@ class ExampleClient(DatagramProtocol):
         self.transport.write(payload)
         self._counter += 1
 
+    def ping_publish_statistics(self) -> None:
+        if len(self._ping_buffer) < 2:
+            return
+        ping_mean = statistics.fmean(self._ping_buffer)
+        ping_max = max(self._ping_buffer)
+        ping_min = min(self._ping_buffer)
+        ping_std = statistics.stdev(self._ping_buffer)
+        print(f"Ping statistics [ms]: Mean:{ping_mean:.6f}\tMax:{ping_max:.6f}\tMin:{ping_min:.6f}\tSTD:{ping_std:.6f}")
+
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser(
@@ -90,7 +107,7 @@ if __name__ == "__main__":
     arg_parser.add_argument("-v", "--verbose", action="store_true")
     arg_parser.add_argument("-r", "--relay-ip", default=YOUR_TURN_IP)
     arg_parser.add_argument("-p", "--relay-port", type=int, default=YOUR_TURN_PORT)
-    arg_parser.add_argument("-f", "--frequency", type=float, default=PING_FREQUENCY)
+    arg_parser.add_argument("-f", "--frequency", type=float, default=PING_DEFAULT_FREQUENCY)
     arg_parser.add_argument("-b", "--bypass", action="store_true", help="Bypass Middleman")
     args = arg_parser.parse_args()
 
